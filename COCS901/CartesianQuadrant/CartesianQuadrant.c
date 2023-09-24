5a4a31a7dfb726edffcc56a59f148a20ce0b479b
Spencer <you@example.com>
2023-09-24 14:10:11 -0500

    Cartesian Quadrant

---
+++ COCS901/CartesianQuadrant/CartesianQuadrant.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+float findQuadrant(float xCord , float yCord)
+{
+    if(xCord==0&&yCord==0)
+    {
+        printf("You are on the orgin.");
+    }
+    if(xCord>0&&yCord>0)
+    {
+        printf("You are in quadrant 1");
+    }
+    if(xCord<0&&yCord>0)
+    {
+        printf("You are in quadrant 2");
+    }
+    if(xCord<0&&yCord<0)
+    {
+        printf("You are in quadrant 3");
+    }
+    if(xCord>0&&yCord<0)
+    {
+        printf("You are in quadrant 4");
+    }
+    if(xCord==0&&yCord!=0)
+    {
+        printf("You are on the X axis");
+    }
+    if(yCord==0&&xCord!=0)
+    {
+        printf("You are on the Y axis");
+    }
+}
+
+int main()
+{
+    float xCord , yCord;
+    printf("Enter your X coordinate: ");
+    scanf("%f",&xCord);
+    printf("Enter your Y coordinate: ");
+    scanf("%f",&yCord);
+
+    findQuadrant(xCord,yCord);
+}
