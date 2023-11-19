feff96e8364ba4eac589a346f801a08f51c17ba9
Spencer <you@example.com>
2023-11-18 21:16:43 -0600

    done

--- COCS901/ElementalPart3/ElementalPart3.c
+++ COCS901/ElementalPart3/ElementalPart3.c
@@ -2,20 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 int main()
 {
-    char readFileData[32]={0};
-    FILE * fp = fopen("elements.txt", "r");
-
-     if(fp == NULL)
-    {
-        printf("Error in opening the file\n");
-        exit(1);
-    }
-
-    fscanf(fp,"%s",readFileData);
-    puts(readFileData);
-    fclose(fp);
-    return 0;
+    char c;
+    FILE *fp = fopen("elements.txt","r");
+    if (fp == NULL) 
+    { 
+        printf("Cannot open file \n"); 
+        exit(0); 
+    } 
+  
+    c = fgetc(fp); 
+    while (c != EOF) 
+    { 
+        printf ("%c", c); 
+        c = fgetc(fp); 
+    } 
+  
+    fclose(fp); 
+    return 0; 
 }
